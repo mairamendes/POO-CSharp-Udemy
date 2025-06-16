fc76c73296a9906b9877b6080f47f7c5b1709e78
Maíra Mendes <109531496+mairamendes@users.noreply.github.com>
2025-06-16 14:07:16 -0300

    Add files via upload

---
+++ ProjetosUdemy/Course/Course/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+        
+        public static double Conversao(double qtdDolar, double cotacaoDolar)
+        {
+            double total = qtdDolar * cotacaoDolar;
+            return total + total * iof / 100.0;
+        }
+    }
+}
---
+++ ProjetosUdemy/Course/Course/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + ", R$ "
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
